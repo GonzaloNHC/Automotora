aa9e733069a08a1a037b70cb64b74935a659a5d1
GonzaloNHC <101850889+GonzaloNHC@users.noreply.github.com>
2022-07-01 12:40:47 -0400

    Update Menu.java

--- src/main/java/gui/Menu.java
+++ src/main/java/gui/Menu.java
@@ -7,11 +7,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Menu extends Ventana implements ActionListener {
-    JButton btnRegistroClientes;
-    JButton btnRegistroVehiculos;
-    JButton btnBusquedaVehiculos;
-    JButton btnVenderVehiculo;
-    JButton btnSalir;
+    JButton RegistroClientes;
+    JButton RegistroVehiculos;
+    JButton BusquedaVehiculos;
+    JButton VenderVehiculo;
+    JButton Salir;
     Automotora automotora;
 
     public Menu(Automotora automotora) {
@@ -19,30 +19,30 @@ public class Menu extends Ventana implements ActionListener {
         JLabel titulo = this.generarEtiqueta("Automotora", 115, 40, 300, 40,
                 "Arial", 30);
 
-        this.btnRegistroClientes = generarBoton("Registrar cliente", 125, 100, 150, 40);
-        this.btnRegistroVehiculos = generarBoton("Registrar vehículo", 125, 180, 150, 40);
-        this.btnBusquedaVehiculos = generarBoton("Buscar vehículo", 125, 260, 150, 40);
-        this.btnVenderVehiculo = generarBoton("Vender vehículo", 125, 340, 150, 40);
-        this.btnSalir = generarBoton("Salir", 125, 420, 150, 40);
+        this.RegistroClientes = generarBoton("Registrar cliente", 125, 100, 150, 40);
+        this.RegistroVehiculos = generarBoton("Registrar vehículo", 125, 180, 150, 40);
+        this.BusquedaVehiculos = generarBoton("Buscar vehículo", 125, 260, 150, 40);
+        this.VenderVehiculo = generarBoton("Vender vehículo", 125, 340, 150, 40);
+        this.Salir = generarBoton("Salir", 125, 420, 150, 40);
 
-        btnRegistroClientes.addActionListener(this);
-        btnRegistroVehiculos.addActionListener(this);
-        btnBusquedaVehiculos.addActionListener(this);
-        btnVenderVehiculo.addActionListener(this);
-        btnSalir.addActionListener(this);
+        RegistroClientes.addActionListener(this);
+        RegistroVehiculos.addActionListener(this);
+        BusquedaVehiculos.addActionListener(this);
+        VenderVehiculo.addActionListener(this);
+        Salir.addActionListener(this);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btnRegistroClientes) {
+        if(e.getSource() == RegistroClientes) {
             VentanaRegistroClientes ventanaRegistroClientes = new VentanaRegistroClientes(this.automotora);
             this.dispose();
-        } else if(e.getSource() == btnRegistroVehiculos) {
+        } else if(e.getSource() == RegistroVehiculos) {
             VentanaRegistroAuto ventanaRegistroVehiculos = new VentanaRegistroAuto(this.automotora);
             this.dispose();
-        } else if(e.getSource() == btnBusquedaVehiculos) {
+        } else if(e.getSource() == BusquedaVehiculos) {
             VentanaBusquedaAuto ventanaBusquedaVehiculos = new VentanaBusquedaAuto(this.automotora);
             this.dispose();
-        } else if(e.getSource() == btnVenderVehiculo) {
+        } else if(e.getSource() == VenderVehiculo) {
             JOptionPane.showMessageDialog(this, "Próximamente", "Mensaje informativo",
                     JOptionPane.INFORMATION_MESSAGE);
         } else {
